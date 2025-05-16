fab056c68de3a1f93b8c5ff6f4adc882db0bdcbd
DrgDodge <dragonul530@gmail.com>
2025-05-17 00:40:01 +0300

    Repairedfr-AppWorking

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,27 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
+
 mod passwords;
+mod totp;
+
+use totp::{add_account, delete_account, get_accounts_with_codes, AppState};
+use std::sync::Mutex;
 
 pub fn run() {
     tauri::Builder::default()
+        .manage(AppState {
+            accounts: Mutex::new(vec![]),
+            next_id: Mutex::new(0),
+        })
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![passwords::passwords])
+        .invoke_handler(tauri::generate_handler![
+            passwords::passwords,
+            add_account,
+            delete_account,
+            get_accounts_with_codes
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,19 +1,6 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod totp;
-
-use totp::{add_account, delete_account, get_accounts_with_codes, AppState};
-use std::sync::Mutex;
-
 fn main() {
-        totlaunloc_lib::run();
-    tauri::Builder::default()
-        .manage(AppState {
-            accounts: Mutex::new(vec![]),
-            next_id: Mutex::new(0),
-        })
-        .invoke_handler(tauri::generate_handler![add_account, delete_account, get_accounts_with_codes])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    totlaunloc_lib::run()
 }
