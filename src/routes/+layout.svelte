c6762398a7d06a91854b81b0ab232ab9f798bf27
DrgDodge <dragonul530@gmail.com>
2025-05-25 22:07:40 +0300

    Test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -42,9 +42,53 @@
       }
     };
   });
+
+
+  import { getCurrentWindow } from '@tauri-apps/api/window';
+
+// when using `"withGlobalTauri": true`, you may use
+// const { getCurrentWindow } = window.__TAURI__.window;
+
+const appWindow = getCurrentWindow();
+
+function minimize(){
+document
+  appWindow.minimize();
+}
+
+function maximize(){
+document
+  appWindow.toggleMaximize();
+}
+
+
+
+document
+  .getElementById('titlebar-maximize')
+  ?.addEventListener('click', () => appWindow.toggleMaximize());
+document
+  .getElementById('titlebar-close')
+  ?.addEventListener('click', () => appWindow.close());
 </script>
 
 <div class="app-container">
+  <div data-tauri-drag-region class="titlebar">
+  <div class="titlebar-button" id="titlebar-minimize" on:click={minimize}>
+    <img
+      src="https://api.iconify.design/mdi:window-minimize.svg"
+      alt="minimize"
+    />
+  </div>
+  <div class="titlebar-button" id="titlebar-maximize">
+    <img
+      src="https://api.iconify.design/mdi:window-maximize.svg"
+      alt="maximize"
+    />
+  </div>
+  <div class="titlebar-button" id="titlebar-close">
+    <img src="https://api.iconify.design/mdi:close.svg" alt="close" />
+  </div>
+</div>
   <aside class="sidebar">
     <nav class="nav-list">
       <a
@@ -339,4 +383,28 @@
     height: 1.5rem;
     filter: brightness(0) invert(1);
   }
+
+  .titlebar {
+  height: 30px;
+  background: #329ea3;
+  user-select: none;
+  display: flex;
+  justify-content: flex-end;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+.titlebar-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 30px;
+  height: 30px;
+  user-select: none;
+  -webkit-user-select: none;
+}
+.titlebar-button:hover {
+  background: #5bbec3;
+}
 </style>
